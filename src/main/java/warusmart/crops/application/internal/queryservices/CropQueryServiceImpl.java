51e255ae77a773c9dca4d8b199fdfc078b5e2a5a
fdaniel13s <u20221c628@upc.edu.pe>
2025-05-07 20:47:40 -0500

    fix: fix errors about references at services implementations.

--- src/main/java/warusmart/crops/application/internal/queryservices/CropQueryServiceImpl.java
+++ src/main/java/warusmart/crops/application/internal/queryservices/CropQueryServiceImpl.java
@@ -5,18 +5,23 @@ import warusmart.crops.domain.model.entities.Care;
 import warusmart.crops.domain.model.queries.GetAllCropsQuery;
 import warusmart.crops.domain.model.queries.GetCaresByCropIdQuery;
 import warusmart.crops.domain.model.queries.GetCropByIdQuery;
-import warusmart.crops.domain.model.services.CropQueryService;
 
-import warusmart.crops.infrastructure.persistence.jpa.repositories.CropRepository;
+
+import warusmart.crops.domain.services.CropQueryService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CropQueryServiceImpl implements CropQueryService {
+public  class CropQueryServiceImpl implements CropQueryService {
     @Override
     public Optional<Crop> handle(GetCropByIdQuery query) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Crop> findById(Long id) {
         return Optional.empty();
     }
 
--- src/main/java/warusmart/crops/domain/services/CropQueryService.java
+++ src/main/java/warusmart/crops/domain/services/CropQueryService.java
@@ -1,7 +1,9 @@
 package warusmart.crops.domain.services;
 
 import warusmart.crops.domain.model.aggregates.Crop;
+import warusmart.crops.domain.model.entities.Care;
 import warusmart.crops.domain.model.queries.GetAllCropsQuery;
+import warusmart.crops.domain.model.queries.GetCaresByCropIdQuery;
 import warusmart.crops.domain.model.queries.GetCropByIdQuery;
 
 import java.util.List;
@@ -10,5 +12,9 @@ import java.util.Optional;
 public interface CropQueryService {
     List<Crop> handle(GetAllCropsQuery query);
     Optional<Crop> handle(GetCropByIdQuery query);
+    //Make this method abstract
+    // and remove the implementation from the class
     Optional<Crop> findById(Long id);
+
+    Optional<Care> handle(GetCaresByCropIdQuery query);
 }
